ad86dde6400f2e969d6fb99918f4b13b7d74f783
Juan Rangel de Prada <juansolobolos@gmail.com>
2023-09-13 20:02:53 +0200

    no message

---
+++ prueba/src/prueba/Principal.java
@@ -0,0 +1,16 @@
+package prueba;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 1;
+		if(a + b == 7) {
+			System.out.println("Te has equivocado maquina");
+		}
+		else{
+			System.out.println("Hello world");
+		}
+	}
+
+}
